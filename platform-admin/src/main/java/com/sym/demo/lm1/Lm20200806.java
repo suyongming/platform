a4a55ec4595ceb2a3329b7cd136abd0691c3c556
liangmin-0830 <liangmin19950830>
2020-08-10 12:10:31 +0800

    测试下本地提交代码。。。

--- platform-admin/src/main/java/com/sym/demo/lm1/Lm20200806.java
+++ platform-admin/src/main/java/com/sym/demo/lm1/Lm20200806.java
@@ -2,6 +2,6 @@ package com.sym.demo.lm1;
 
 public class Lm20200806 {
     public static void main(String[] args) {
-        System.out.println("aaaaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaaaa啊啊啊啊");
     }
 }
