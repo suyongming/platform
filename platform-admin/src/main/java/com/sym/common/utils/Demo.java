82ce68b0dd2aedd628f262c0ff8a47e05ab84784
suyongming <suyongming@1183768169179433.onaliyun.com>
2021-08-31 10:43:49 +0800

    爬虫等

---
+++ platform-admin/src/main/java/com/sym/common/utils/Demo.java
@@ -0,0 +1,66 @@
+package com.sym.common.utils;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.sym.demo.entity.Person;
+
+import java.util.*;
+
+/**
+ * @description
+ * @Author: sym
+ * @Date: 2021/8/17 17:20
+ */
+public class Demo {
+    public static void main(String[] args) {
+        // 下面数据中有两个 开发人员叫 Addison 的, 因为是jdk17 这样去重
+        Set<String> firstNames = new HashSet<>();
+        // 去重后的集合
+        List<Person> distinctList = new ArrayList<>();
+        System.out.println("原本有" + javaProgrammers.size() + "条");
+
+        // 普通for,记得对数组判空
+        for (int i = 0; i < javaProgrammers.size(); i++) {
+            Person person = javaProgrammers.get(i);
+            if(firstNames.add(person.getFirstName())) {
+                distinctList.add(person);
+            }
+        }
+
+        System.out.println("根据条件去重后" + distinctList.size() + "条");
+
+
+    }
+
+    private static List<Person> javaProgrammers = new ArrayList<Person>() {
+        {
+            add(new Person("Addison", "Jaycob", "Java programmer", "male", 43, 2000));
+            add(new Person("Tamsen", "Brittany", "Java programmer", "female", 23, 1500));
+            add(new Person("Floyd", "Donny", "Java programmer", "male", 33, 1800));
+            add(new Person("Sindy", "Jonie", "Java programmer", "female", 32, 1600));
+            add(new Person("Vere", "Hervey", "Java programmer", "male", 22, 1200));
+            add(new Person("Maude", "Jaimie", "Java programmer", "female", 27, 1900));
+            add(new Person("Shawn", "Randall", "Java programmer", "male", 30, 2300));
+            add(new Person("Jayden", "Corrina", "Java programmer", "female", 35, 1700));
+            add(new Person("Palmer", "Dene", "Java programmer", "male", 33, 2000));
+            add(new Person("Addison", "Pam", "Java programmer", "female", 34, 1300));
+        }
+    };
+
+    private static List<Person> phpProgrammers = new ArrayList<Person>() {
+        {
+            add(new Person("Elsdon", "Jaycob", "PHP programmer", "male", 34, 1550));
+            add(new Person("Ethan", "Cicely", "PHP programmer", "female", 23, 1200));
+            add(new Person("Elsdon", "Channing", "PHP programmer", "male", 32, 1600));
+            add(new Person("Tori", "Sheryl", "PHP programmer", "female", 21, 1000));
+            add(new Person("Floyd", "Shad", "PHP programmer", "male", 32, 1100));
+            add(new Person("Rosalind", "Layla", "PHP programmer", "female", 25, 1300));
+            add(new Person("Fraser", "Hewie", "PHP programmer", "male", 36, 1100));
+            add(new Person("Quinn", "Tamara", "PHP programmer", "female", 21, 1000));
+            add(new Person("Elsdon", "Jaycob", "PHP programmer", "male", 38, 1600));
+            add(new Person("Evonne", "Shari", "PHP programmer", "female", 40, 1800));
+        }
+    };
+
+
+}
--- platform-admin/src/main/java/com/sym/modules/sys/service/impl/SysMenuServiceImpl.java
+++ platform-admin/src/main/java/com/sym/modules/sys/service/impl/SysMenuServiceImpl.java
@@ -14,6 +14,7 @@ import com.sym.modules.sys.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
